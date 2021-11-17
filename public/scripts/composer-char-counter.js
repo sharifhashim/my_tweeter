5bdd2ae7bcede2209b98cea3e2f51c2736218e72
Hashim Sharif <sharif.hashim@gmail.com>
2021-11-17 19:22:49 +0000

    remove console.log statement

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
     const $counter = $(this).val().length
     $(this).siblings(".tweetbtn").children(".counter").html(140 - $counter)
     const $maxLength = $(this).siblings(".tweetbtn").children(".counter").html()
-    console.log($maxLength)
     if ($maxLength < 0) {
       $(this).siblings(".tweetbtn").children(".counter").addClass("negativeCount")
     } else {
