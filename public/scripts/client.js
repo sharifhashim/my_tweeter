4a06d934d9d2efee64bb7f0dbddfc3d1c6dbb9a5
Hashim Sharif <sharif.hashim@gmail.com>
2021-11-17 21:22:42 +0000

    add ajax get request to fetch tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,30 +5,30 @@
  */
 
 $(document).ready(function() {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ]
 //   const tweetData = {
 //     "user": {
 //       "name": "Newton",
@@ -75,6 +75,24 @@ $(document).ready(function() {
     return $tweet
 
   }
-  renderTweets(data);
+  //renderTweets(data);
 
+
+  $("form").on("submit", function(event) {
+    event.preventDefault();
+    console.log("default event stopped")
+
+    $.post("/tweets", $("#tweet-text").serialize())
+  })
+
+  const loadTweets = function() {
+    $.ajax({
+      url: "http://localhost:8080/tweets", 
+      method: "GET",
+    })
+    .done((result) => {
+      renderTweets(result)
+    })
+  }
+  loadTweets()
 })
