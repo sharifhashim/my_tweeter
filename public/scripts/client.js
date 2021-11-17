2455d69b1d64d94b57401996aa42256a6ee8355d
Hashim Sharif <sharif.hashim@gmail.com>
2021-11-17 22:51:39 +0000

    add function to append last element in array and send get request without refreshing page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,48 +5,16 @@
  */
 
 $(document).ready(function() {
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png"
-  //       ,
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   }
-  // ]
-//   const tweetData = {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png",
-//         "handle": "@SirIsaac"
-//       },
-//     "content": {
-//         "text": "If I have seen further it is by standing on the shoulders of giants"
-//       },
-//     "created_at": 1461116232227
-//  }
 
   const renderTweets = function(arr) {
     for (const item of arr) {
-      $(`.container`).append(createTweetElement(item))
+      $(`.prepend`).prepend(createTweetElement(item))
     }
   }
-
+  const prePendTweet = function(arr) {
+    const lastItem = arr[arr.length - 1]
+    $(`.prepend`).prepend(createTweetElement(lastItem))
+  }
   const createTweetElement = function(tweetData) {
     const $tweet = $(`<article></article>`)
     const $header = $(`
@@ -82,7 +50,6 @@ $(document).ready(function() {
     event.preventDefault();
     console.log("default event stopped")
 
-    //$.post("/tweets", $("#tweet-text").serialize())
     if ($("#tweet-text").val() === "" || $("#tweet-text").val() === null) {
       return alert("Please fill out tweet")
     }
@@ -93,6 +60,15 @@ $(document).ready(function() {
       method: "POST",
       url: "/tweets",
       data: $("#tweet-text").serialize(),
+    })
+    .then(() => {
+      $.ajax({
+        url: "http://localhost:8080/tweets", 
+        method: "GET",
+      })
+      .done((result) => {
+        prePendTweet(result)
+      })
     })
   })
 
