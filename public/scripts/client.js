dcc2e546c82ccd79f69253dd266246381faac9aa
Hashim Sharif <sharif.hashim@gmail.com>
2021-11-19 01:09:50 +0000

    correct slide up and slide down functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,15 +8,15 @@ $(document).ready(function() {
 
   const renderTweets = function(arr) {
     for (const item of arr) {
-      $(`.prepend`).prepend(createTweetElement(item))
+      $(`.prepend`).prepend(createTweetElement(item));
     }
-  }
+  };
   const prePendTweet = function(arr) {
-    const $lastItem = arr[arr.length - 1]
-    $(`.prepend`).prepend(createTweetElement($lastItem))
-  }
+    const $lastItem = arr[arr.length - 1];
+    $(`.prepend`).prepend(createTweetElement($lastItem));
+  };
   const createTweetElement = function(tweetData) {
-    const $tweet = $(`<article></article>`)
+    const $tweet = $(`<article></article>`);
     const $header = $(`
     <header>
       <div class="faceIcon">
@@ -24,10 +24,10 @@ $(document).ready(function() {
         <span>${tweetData.user.name}</span>
       </div>
       <span class="grey">${tweetData.user.handle}</span>
-    </header>`)
-    const $textareaTweet = tweetData.content.text
-    const $tweetBody = $(`<p></p>`).text($textareaTweet)
-    const $timeAgo = timeago.format(tweetData.created_at)
+    </header>`);
+    const $textareaTweet = tweetData.content.text;
+    const $tweetBody = $(`<p></p>`).text($textareaTweet);
+    const $timeAgo = timeago.format(tweetData.created_at);
     const $footer = $(`
     <footer>
       <span>${$timeAgo}</span>
@@ -36,34 +36,36 @@ $(document).ready(function() {
         <span><i class="fa-solid fa-retweet"></i></span>
         <span><i class="fa-solid fa-heart"></i></span>
       </div>
-    </footer>`)
-    $tweet.append($header)
-    $tweet.append($tweetBody)
-    $tweet.append($footer)
+    </footer>`);
+    $tweet.append($header);
+    $tweet.append($tweetBody);
+    $tweet.append($footer);
 
-    return $tweet
+    return $tweet;
 
-  }
-  //renderTweets(data);
-  $(`.errorMsg`).hide()
+  };
+  
+  $(`.errorMsg`).hide();
 
   $("form").on("submit", function(event) {
     event.preventDefault();
-    console.log("default event stopped")
-    $(`.errorMsg`).slideUp(200)
     if ($("#tweet-text").val() === "" || $("#tweet-text").val() === null) {
-      setTimeout(function() {
-        $(`.insertError`).text("Please fill out tweet")
-      }, 200)
-      return $(`.errorMsg`).slideDown("slow").show()
+      $(`.insertError`).text("Please fill out tweet");
+      return $(`.errorMsg`).slideDown(() => {
+        setTimeout(() => {
+          $(`.errorMsg`).slideUp();
+        }, 2000);
+      });
     }
-    if ($("#tweet-text").val().length > 140) {
-      setTimeout(function() {
-        $(`.insertError`).text("Tweet too long. Please limit to 140 characters or less")
-      }, 200)
-      return $(`.errorMsg`).slideDown("slow").show()
+    else if ($("#tweet-text").val().length > 140) {
+      $(`.insertError`).text("Tweet too long. Please limit to 140 characters or less");
+      return $(`.errorMsg`).slideDown(() => {
+        setTimeout(() => {
+          $(`.errorMsg`).slideUp();
+        }, 2000);
+      });
     }
-    const $tweetText = $("#tweet-text")
+    const $tweetText = $("#tweet-text");
     
     $.ajax({
       method: "POST",
@@ -72,23 +74,25 @@ $(document).ready(function() {
     })
     .then(() => {
       $.ajax({
-        url: "http://localhost:8080/tweets", 
+        url: "http://localhost:8080/tweets",
         method: "GET",
       })
       .done((result) => {
-        prePendTweet(result)
-      })
-    })
-  })
+        prePendTweet(result);
+      });
+    });
+    $("#tweet-text").val('');
+    $(`.counter`).val('140');
+  });
 
   const loadTweets = function() {
     $.ajax({
-      url: "http://localhost:8080/tweets", 
+      url: "http://localhost:8080/tweets",
       method: "GET",
     })
     .done((result) => {
-      renderTweets(result)
-    })
-  }
-  loadTweets()
-})
+      renderTweets(result);
+    });
+  };
+  loadTweets();
+});
