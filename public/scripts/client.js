4856ba8ff212cc34edfbdfa8d2b08db8c9c73234
Hashim Sharif <sharif.hashim@gmail.com>
2021-11-19 15:10:30 +0000

    add comments to code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,16 +5,18 @@
  */
 
 $(document).ready(function() {
-
+  // Loops through array of objects to get data from tweets and pass them into another function
   const renderTweets = function(arr) {
     for (const item of arr) {
       $(`.prepend`).prepend(createTweetElement(item));
     }
   };
+  // Gets the lastest tweet and prepends to top of html section $(`.prepend)
   const prePendTweet = function(arr) {
     const $lastItem = arr[arr.length - 1];
     $(`.prepend`).prepend(createTweetElement($lastItem));
   };
+  // Access tweet object and create elemets around that data to insert into html
   const createTweetElement = function(tweetData) {
     const $tweet = $(`<article></article>`);
     const $header = $(`
@@ -44,11 +46,12 @@ $(document).ready(function() {
     return $tweet;
 
   };
-  
+  // Hide html error message on intial page load
   $(`.errorMsg`).hide();
-
+  // Event listener attacted to form element 
   $("form").on("submit", function(event) {
     event.preventDefault();
+    // If textarea empty send error message 
     if ($("#tweet-text").val() === "" || $("#tweet-text").val() === null) {
       $(`.insertError`).text("Please fill out tweet");
       return $(`.errorMsg`).slideDown(() => {
@@ -57,6 +60,7 @@ $(document).ready(function() {
         }, 2000);
       });
     }
+    // If textarea over 140 characters send error message
     else if ($("#tweet-text").val().length > 140) {
       $(`.insertError`).text("Tweet too long. Please limit to 140 characters or less");
       return $(`.errorMsg`).slideDown(() => {
@@ -66,7 +70,7 @@ $(document).ready(function() {
       });
     }
     const $tweetText = $("#tweet-text");
-    
+    // If tweet passes validation send the tweet to /tweets (post request) after post request send a Get request to update page with new tweet
     $.ajax({
       method: "POST",
       url: "/tweets",
@@ -81,10 +85,12 @@ $(document).ready(function() {
         prePendTweet(result);
       });
     });
+    // Reset text area 
     $("#tweet-text").val('');
+    // Reset charachter counter
     $(`.counter`).val('140');
   });
-
+  // Get request to load any tweets stored at /tweets on page load
   const loadTweets = function() {
     $.ajax({
       url: "http://localhost:8080/tweets",
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
-
+  // Attached event listener to textarea 
   $("#tweet-text").on("input", function() {
     const $counter = $(this).val().length
+    // update character counter when text is input into textarea
     $(this).siblings(".tweetbtn").children(".counter").html(140 - $counter)
     const $maxLength = $(this).siblings(".tweetbtn").children(".counter").html()
+    // If textarea exceeds limit of 140 characters apply class to give red color
     if ($maxLength < 0) {
       $(this).siblings(".tweetbtn").children(".counter").addClass("negativeCount")
     } else {
